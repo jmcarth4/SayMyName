2b7bcf4a418a4d4edbfa85a2bc71493748a8c47a
jmcarth4@gmail.com <jmcarth4@gmail.com>
2022-01-21 17:07:58 -0700

    Add project files.

---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,26 @@
+﻿'Jessica McArthur
+'RCET0265
+'Spring 2020
+'Say My Name
+'UrleSFSdf;j
+
+Option Explicit On
+Option Strict On
+
+Module SayMyName
+    Sub Main()
+        'variables declared 
+        Dim userInput As String
+
+        'user is asked their name.
+        Console.WriteLine("Quelle est votre nom? Comment t'appelle tu? {Greetings earthling! What do they refer to you as?}")
+        userInput = Console.ReadLine()
+
+        'user is greatted by their name
+        Console.WriteLine("Salut! Bonjour! Bonsoir! Bonjouree! " & userInput & " Go buy a flying monkey")
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
